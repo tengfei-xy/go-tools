961772144bfe62b3d04c7ee33d3a62060c5bbc18
浔阳陌客 <61930212+tengfei-xy@users.noreply.github.com>
2023-08-01 19:36:27 +0800

    添加系统相关代码

---
+++ system.go
@@ -0,0 +1,22 @@
+package tools
+
+import (
+	"os"
+	"path/filepath"
+	"runtime"
+)
+
+// 作用:返回程序的文件的所在路径
+func GetAppPath() string {
+	var abWorkPath string
+	if runtime.GOOS == "windows" {
+		exePath, _ := os.Executable()
+		abWorkPath, _ = filepath.EvalSymlinks(filepath.Dir(exePath))
+	} else {
+		_, filename, _, ok := runtime.Caller(0)
+		if ok {
+			abWorkPath = filepath.Dir(filename)
+		}
+	}
+	return abWorkPath
+}
